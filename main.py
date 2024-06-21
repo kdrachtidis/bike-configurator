4e07840967bb8d38851a514514ff29c16c2293ac
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2024-06-21 11:14:29 +0200

    http added

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 from db import engine
-from routers import cars
-app = FastAPI(title="Bike Configurator App")
+from routers import cars, web
+from routers.cars import BadTripException
+
+app = FastAPI(title="Car Sharing")
+app.include_router(web.router)
 app.include_router(cars.router)
+
+
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
@@ -13,5 +33,21 @@ def on_startup():
     SQLModel.metadata.create_all(engine)
 
 
+@app.exception_handler(BadTripException)
+async def unicorn_exception_handler(request: Request, exc: BadTripException):
+    return JSONResponse(
+        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"message": "Bad Trip"},
+    )
+
+
+@app.middleware("http")
+async def add_cars_cookie(request: Request, call_next):
+    response = await call_next(request)
+    response.set_cookie(key="cars_cookie",
+                        value="you_visited_the_carsharing_app")
+    return response
+
+
 if __name__ == "__main__":
-    uvicorn.run("configurator:app", reload=True)
+    uvicorn.run("main:app", reload=True)
