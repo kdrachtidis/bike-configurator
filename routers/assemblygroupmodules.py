088399d35c3e95508b46c1de71238a6093fae401
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2024-09-01 11:23:50 +0200

    Update assemblygroupmodules.py

--- routers/assemblygroupmodules.py
+++ routers/assemblygroupmodules.py
@@ -2,38 +2,48 @@ from fastapi import Depends, HTTPException, APIRouter
 from sqlmodel import Session, select
 
 from db import get_session
-from schemas import AssemblyGroup, AssemblyGroupInput, AssemblyGroupOutput, AssemblyGroupModule, AssemblyGroupModuleInput, AssemblyGroupModuleOutput
+from schemas import AssemblyGroup, AssemblyGroupModule, AssemblyGroupModuleInput, AssemblyGroupModuleOutput, AssemblyGroupOutput
 
-router = APIRouter(prefix="/api/modules")
+router = APIRouter(prefix="/api/assemblygroups")
 
 
 class BadTripException(Exception):
     pass
 
-# Add module
+# Add module dependent on assembly group
 
 
-@router.post("/{assemblygroup_id}/groupmodules", response_model=AssemblyGroupModule, tags=["Group Modules"])
-def add_group_module(assemblygroup_id: int, module_input: AssemblyGroupModuleInput, session: Session = Depends(get_session)) -> AssemblyGroupModule:
+@router.post("/{assemblygroup_id}/assemblygroupmodules", response_model=AssemblyGroupModule, tags=["Group Modules"])
+def add_group_module(assemblygroup_id: int, assemblygroupmodule_input: AssemblyGroupModuleInput, session: Session = Depends(get_session)) -> AssemblyGroupModule:
     assemblygroup = session.get(AssemblyGroup, assemblygroup_id)
     if assemblygroup:
-        new_module = AssemblyGroupModule.from_orm(
-            module_input, update={'assemblygroup_id': assemblygroup_id})
-        assemblygroup.groupmodules.append(new_module)
+        new_assemblygroupmodule = AssemblyGroupModule.from_orm(
+            assemblygroupmodule_input, update={'assemblygroup_id': assemblygroup_id})
+        assemblygroup.groupmodules.append(new_assemblygroupmodule)
         session.commit()
-        session.refresh(new_module)
-        return new_module
+        session.refresh(new_assemblygroupmodule)
+        return new_assemblygroupmodule
     else:
         raise HTTPException(status_code=404, detail=f"No module with id={id}.")
 
 # Get group module by id
 
 
-@router.get("/{id}", response_model=AssemblyGroupModuleOutput, tags=["Group Modules"])
-def get_module_by_id(id: int, session: Session = Depends(get_session)) -> AssemblyGroupModule:
-    groupmodule = session.get(AssemblyGroupModule, id)
-    if groupmodule:
-        return groupmodule
+# @router.get("/{id}", response_model=AssemblyGroupModuleOutput, tags=["Group Modules"])
+# def get_module_by_id(id: int, session: Session = Depends(get_session)) -> AssemblyGroupModule:
+ #   groupmodule = session.get(AssemblyGroupModule, id)
+  #  if groupmodule:
+   #     return groupmodule
+    # else:
+     #   raise HTTPException(
+      #      status_code=404, detail=f"No group module with id{id}.")
+
+@router.get("/{assemblygroup_id}/assemblygroupmodules", response_model=AssemblyGroupOutput, tags=["Group Modules"])
+def get_assembly_modules(assemblygroup_id: int, session: Session = Depends(get_session)) -> AssemblyGroupModule:
+    # groupmodule = session.get(AssemblyGroupModule, id)
+    assemblygroup = session.get(AssemblyGroup, assemblygroup_id)
+    if assemblygroup:
+        assemblygroup.groupmodules.all()
     else:
         raise HTTPException(
             status_code=404, detail=f"No group module with id{id}.")
