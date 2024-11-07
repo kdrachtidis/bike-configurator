4eb643a671c46f71c5963825ec3f8b0dff65bac3
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2024-11-07 10:46:01 +0100

    Get al modules added

--- routers/assemblygroupmodules.py
+++ routers/assemblygroupmodules.py
@@ -5,7 +5,7 @@ from routers.auth import get_current_user
 from db import get_session
 from schemas import AssemblyGroup, AssemblyGroupModule, AssemblyGroupModuleInput, AssemblyGroupModuleOutput, AssemblyGroupOutput
 
-router = APIRouter(prefix="/api/assemblygroups")
+router = APIRouter()
 
 # Reusable components
 msg_tags = "Assembly Group Modules"
@@ -15,10 +15,9 @@ msg_description_post = "Add an assembly group module, providing the ID of the as
 def msg_no_item(i):
     return f"No assembly group with id={i}."
 
-# Add module dependent on assembly group
+# Add module assigned to assembly group
 
-
-@router.post("/{assemblygroup_id}/assemblygroupmodules", response_model=AssemblyGroupModule, tags=[msg_tags], description=msg_description_post)
+@router.post("/api/assemblygroups/{assemblygroup_id}/assemblygroupmodules", response_model=AssemblyGroupModule, tags=[msg_tags], description=msg_description_post)
 def add_group_module(assemblygroup_id: int, assemblygroupmodule_input: AssemblyGroupModuleInput, session: Session = Depends(get_session)) -> AssemblyGroupModule:
     assemblygroup = session.get(AssemblyGroup, assemblygroup_id)
     if assemblygroup:
@@ -32,11 +31,20 @@ def add_group_module(assemblygroup_id: int, assemblygroupmodule_input: AssemblyG
         raise HTTPException(
             status_code=404, detail=msg_no_item(assemblygroup_id))
 
-# @router.get("/{assemblygroup_id}/assemblygroupmodules/{assemblygroupmodule_id}", response_model=AssemblyGroupModule, tags=[msg_tags])
-# def get_group_module(assemblygroup_id: int, assemblygroupmodule_id: int, session: Session = Depends(get_session)) -> AssemblyGroupModule:
-#    assemblygroupmodule = {"assemblygroup_id": assemblygroup_id, "assemblygroupmodule_id": assemblygroupmodule_id}
-#    if assemblygroupmodule:
-#        return assemblygroupmodule
-#    else:
-#        raise HTTPException(
-#            status_code=404, detail=msg_no_item(id))
+# [Temporary] Get all assembly group modules
+
+@router.get("/api/assemblygroupmodules", tags=[msg_tags])
+def get_group_modules(session: Session = Depends(get_session)) -> list:
+    query = select(AssemblyGroupModule)
+    return session.exec(query).all()
+
+# Get assembly group module based on group ID
+
+@router.get("/api/assemblygroups/{assemblygroup_id}/assemblygroupmodules/{assemblygroupmodule_id}", response_model=AssemblyGroupModuleOutput, tags=[msg_tags])
+def get_group_module_by_id(assemblygroup_id: int, assemblygroupmodule_id: int, session: Session = Depends(get_session)) -> AssemblyGroup:
+    module = session.get(AssemblyGroupModule, assemblygroup_id, assemblygroupmodule_id)
+    if module:
+        return module
+    else:
+         raise HTTPException(
+            status_code=404, detail=msg_no_item(id))
