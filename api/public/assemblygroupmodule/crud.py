bb4f69804b8ed26ba2e0b6de4c3f1bada81fb3c0
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2025-01-11 23:25:15 +0100

    CRUD for Assembly-group-module

---
+++ api/public/assemblygroupmodule/crud.py
@@ -0,0 +1,82 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException
+from sqlmodel import Session, select
+
+from db import get_session
+from api.auth.views import get_current_user
+from api.auth.models import User
+from api.public.assemblygroup.models import AssemblyGroup
+from api.public.assemblygroupmodule.models import AssemblyGroupModule, AssemblyGroupModuleInput, AssemblyGroupModuleOutput
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+# HTTPException details messages
+
+
+def msg_no_group(i):
+    return f"No assembly group with id={i}."
+
+
+def msg_no_module(i):
+    return f"No assembly group module with id={i}."
+
+# Create
+
+
+def create_assemblygroupmodule(id: int, input: AssemblyGroupModuleInput, session: SessionDep, user: User = Depends(get_current_user)) -> AssemblyGroupModule:
+    assemblygroup = session.get(AssemblyGroup, id)
+    if assemblygroup:
+        assemblygroupmodule = AssemblyGroupModule.model_validate(input)
+        # session.add(module)
+        assemblygroup.assemblygroupmodules.append(assemblygroupmodule)
+        session.commit()
+        session.refresh(assemblygroupmodule)
+        return assemblygroupmodule
+    else:
+        raise HTTPException(
+            status_code=404, detail=msg_no_group(id)
+        )
+
+# Read
+
+
+def read_all_assemblygroupmodules(session: SessionDep) -> list:
+    query = select(AssemblyGroupModule)
+    print("Read all assemby group modules.")
+    return session.exec(query).all()
+
+
+def read_assemblygroupmodule(id: int, session: SessionDep) -> AssemblyGroupModule:
+    assemblygroupmodule = session.get(AssemblyGroupModule, id)
+    if assemblygroupmodule:
+        print("Read assembly group module with id=", id)
+        return assemblygroupmodule
+    else:
+        raise HTTPException(
+            status_code=404, detail=msg_no_module(id))
+
+# Update
+
+
+def update_assemblygroupmodule(id: int, input: AssemblyGroupModuleInput, session: SessionDep, user: User = Depends(get_current_user)) -> AssemblyGroupModule:
+    assemblygroupmodule = session.get(AssemblyGroupModule, id)
+    if assemblygroupmodule:
+        assemblygroupmodule.name = input.name
+        session.commit()
+        return assemblygroupmodule
+    else:
+        raise HTTPException(status_code=404, detail=msg_no_module(id))
+
+# Delete
+
+
+def delete_assemblygroupmodule(id: int, session: SessionDep, user: User = Depends(get_current_user)) -> None:
+    assemblygroupmodule = session.get(AssemblyGroupModule, id)
+    if assemblygroupmodule:
+        session.delete(assemblygroupmodule)
+        session.commit()
+    else:
+        raise HTTPException(
+            status_code=404, detail=msg_no_module(id)
+        )
--- api/public/assemblygroupmodule/views.py
+++ api/public/assemblygroupmodule/views.py
@@ -1,18 +1,18 @@
 from typing import Annotated
 
-from fastapi import Depends, HTTPException, APIRouter, status
-from sqlmodel import Session, select
+from fastapi import Depends, APIRouter, status
+from sqlmodel import Session
 
-from routers.auth import get_current_user
+from api.auth.views import get_current_user
 from db import get_session
+from api.public.assemblygroupmodule.crud import create_assemblygroupmodule, read_all_assemblygroupmodules, read_assemblygroupmodule, update_assemblygroupmodule, delete_assemblygroupmodule
 from api.auth.models import User
-from api.public.assemblygroup.models import AssemblyGroup
 from api.public.assemblygroupmodule.models import AssemblyGroupModule, AssemblyGroupModuleInput, AssemblyGroupModuleOutput
 
 router = APIRouter()
 SessionDep = Annotated[Session, Depends(get_session)]
 
-# Reusable components
+# Swagger UI's descriptions
 msg_tags = "Assembly Group Module"
 msg_tags_id = "Assembly Group Module (by ID)"
 msg_description_post = "Add an assembly group module, providing the ID of the assembly group it belongs to."
@@ -22,55 +22,26 @@ msg_description_get_group_id = "Get a specific assembly group module based on it
 msg_description_delete = "Remove a specific assembly group module based on its ID."
 msg_description_put = "Edit a specific assembly group module based on its ID."
 
-
-def msg_success():
-    return f"Assembly group module created successfully."
-
-
-def msg_no_group(i):
-    return f"No assembly group with id={i}."
-
-
-def msg_no_module(i):
-    return f"No assembly group module with id={i}."
-
 # Get all assembly group modules
 
 
 @router.get("/assemblygroupmodules/", tags=[msg_tags], description=msg_description_get, status_code=status.HTTP_200_OK)
-def get_group_modules(session: SessionDep) -> list:
-    query = select(AssemblyGroupModule)
-    return session.exec(query).all()
+def get_group_modules(session: SessionDep):
+    return read_all_assemblygroupmodules(session=session)
 
 # Get an assembly group module based on group ID
 
 
 @router.get("/assemblygroupmodules/{id}", response_model=AssemblyGroupModuleOutput, tags=[msg_tags_id], description=msg_description_get_group_id)
 def get_group_module_by_group_id(id: int, session: SessionDep) -> AssemblyGroupModule:
-    assemblygroupmodule = session.get(AssemblyGroupModule, id)
-    if assemblygroupmodule:
-        return assemblygroupmodule
-    else:
-        raise HTTPException(
-            status_code=404, detail=msg_no_module(id))
+    return read_assemblygroupmodule(id=id, session=session)
 
 # Add module assigned to assembly group
 
 
 @router.post("/assemblygroups/{assemblygroup_id}/assemblygroupmodules/", response_model=AssemblyGroupModule, tags=[msg_tags], description=msg_description_post, status_code=status.HTTP_201_CREATED)
-def add_group_module(assemblygroup_id: int, input: AssemblyGroupModuleInput, session: SessionDep, user: User = Depends(get_current_user)) -> AssemblyGroupModule:
-    assemblygroup = session.get(AssemblyGroup, assemblygroup_id)
-    if assemblygroup:
-        assemblygroupmodule = AssemblyGroupModule.model_validate(input)
-        # session.add(module)
-        assemblygroup.assemblygroupmodules.append(assemblygroupmodule)
-        session.commit()
-        session.refresh(assemblygroupmodule)
-        return assemblygroupmodule
-    else:
-        raise HTTPException(
-            status_code=404, detail=msg_no_group(assemblygroup_id)
-        )
+def add_group_module(id: int, input: AssemblyGroupModuleInput, session: SessionDep, user: User = Depends(get_current_user)) -> AssemblyGroupModule:
+    return create_assemblygroupmodule(id=id, input=input, session=session, user=user)
 
 
 # Delete an assembly group module based on group ID
@@ -78,24 +49,11 @@ def add_group_module(assemblygroup_id: int, input: AssemblyGroupModuleInput, ses
 
 @router.delete("/assemblygroupmodules/{id}", tags=[msg_tags_id], description=msg_description_delete, status_code=status.HTTP_200_OK)
 def remove_assemby_group_module(id: int, session: SessionDep, user: User = Depends(get_current_user)) -> None:
-    assemblygroupmodule = session.get(AssemblyGroupModule, id)
-    if assemblygroupmodule:
-        session.delete(assemblygroupmodule)
-        session.commit()
-    else:
-        raise HTTPException(
-            status_code=404, detail=msg_no_module(id)
-        )
+    return delete_assemblygroupmodule(id=id, session=session, user=user)
 
 # Edit assembly group module
 
 
 @router.put("/assemblygroupmodules/{id}", tags=[msg_tags_id], description=msg_description_put, status_code=status.HTTP_200_OK)
-def edit_assembly_group_module(id: int, new_assemblygroupmodule: AssemblyGroupModuleInput, session: SessionDep, user: User = Depends(get_current_user)) -> AssemblyGroupModule:
-    assemblygroupmodule = session.get(AssemblyGroupModule, id)
-    if assemblygroupmodule:
-        assemblygroupmodule.name = new_assemblygroupmodule.name
-        session.commit()
-        return assemblygroupmodule
-    else:
-        raise HTTPException(status_code=404, detail=msg_no_module(id))
+def edit_assembly_group_module(id: int, input: AssemblyGroupModuleInput, session: SessionDep, user: User = Depends(get_current_user)) -> AssemblyGroupModule:
+    return update_assemblygroupmodule(id=id, input=input, session=session, user=user)
