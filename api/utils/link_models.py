118adf9b6356849923e49e7ec59f95fc6037d9fe
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2025-01-14 23:47:32 +0100

    Modules to Components added

--- api/utils/link_models.py
+++ api/utils/link_models.py
@@ -3,13 +3,26 @@ from sqlmodel import SQLModel, Field
 
 class GroupsToModules(SQLModel, table=True):
     group_id: int | None = Field(
-        default=None, foreign_key="assemblygroup.id", primary_key=True)
+        default=None, foreign_key="assemblygroup.id", primary_key=True
+    )
     modude_id: int | None = Field(
-        default=None, foreign_key="assemblygroupmodule.id", primary_key=True)
+        default=None, foreign_key="assemblygroupmodule.id", primary_key=True
+    )
 
 
 class TypesToGroups(SQLModel, table=True):
     type_id: int | None = Field(
-        default=None, foreign_key="biketype.id", primary_key=True)
+        default=None, foreign_key="biketype.id", primary_key=True
+    )
     group_id: int | None = Field(
-        default=None, foreign_key="assemblygroup.id", primary_key=True)
+        default=None, foreign_key="assemblygroup.id", primary_key=True
+    )
+
+
+class ModulesToComponents(SQLModel, table=True):
+    module_id: int | None = Field(
+        default=None, foreign_key="assemblygroupmodule.id", primary_key=True
+    )
+    component_id: int | None = Field(
+        default=None, foreign_key="bikecomponent.id", primary_key=True
+    )
