e1a55be540489e6b44bbe413a835970c27fc5a04
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2025-02-20 12:49:10 +0100

    Update create_content.py
    Fix

--- create_content.py
+++ create_content.py
@@ -11,14 +11,14 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
+
 def create_demo_content():
     create_BikeTypes()
     create_AssemblyGroups()
     create_AssemblyGroupModules()
     create_BikeComponents()
-    yield
+
 
 if __name__ == "__main__":
     SQLModel.metadata.create_all(engine)
     create_demo_content()
-    
