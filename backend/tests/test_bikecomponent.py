83301c9554068a3f2ff3111bf7a54a7484338e85
Konstantinos Drachtidis <kdrachtidis@outlook.com>
2025-09-17 22:45:12 +0200

    Update test_bikecomponent.py

--- backend/tests/test_bikecomponent.py
+++ backend/tests/test_bikecomponent.py
@@ -1,41 +1,46 @@
 from unittest.mock import Mock
 from fastapi.testclient import TestClient
 
-from backend.src.main import app
-from src.views.user import User
+from src.main import app
 from src.models.bikecomponent import BikeComponentInput, BikeComponent
-from src.views.bikecomponent import create_a_bike_component
+from src.models.assemblygroupmodule import AssemblyGroupModule
+from src.crud.bikecomponent import create_bikecomponent
 
 client = TestClient(app)
 
-
-def test_read_all_bikecomponents():
-    response = client.get("/api/bikecomponents/")
-    assert response.status_code == 200
-    bikecomponents = response.json()
-    assert all(["source" in c for c in bikecomponents])
-    assert all(["group" in c for c in bikecomponents])
-
-
-def test_read_bikecomponent():
-    response = client.get("/api/bikecomponents/1")
-    assert response.status_code == 200
-    bikecomponent = response.json()
-    assert "name" in bikecomponent
-    assert "id" in bikecomponent
+class TestBikeComponentRead:
+    def test_read_all_bikecomponents(self):
+        response = client.get("/api/bikecomponents/")
+        assert response.status_code == 200
+        bikecomponents = response.json()
+        assert all(["source" in c for c in bikecomponents])
+        assert all(["group" in c for c in bikecomponents])
 
 
-# def test_add_bikecomponent_with_mock_session():
-#     mock_session = Mock()
-#     input = BikeComponentInput(name="TestBikeComponent", source="TestSource", price=100, group="TestGroup", id="1")
-#     user = User(username="TestUser")
-#     result = create_a_bike_component(id="1", input=input, session=mock_session, user=user)
+    def test_read_bikecomponent(self):
+        response = client.get("/api/bikecomponents/1")
+        assert response.status_code == 200
+        bikecomponent = response.json()
+        assert "name" in bikecomponent
+        assert "id" in bikecomponent
 
-#     mock_session.add.assert_called_once()
-#     mock_session.commit.assert_called_once()
-#     mock_session.refresh.assert_called_once()
-#     assert isinstance(result, BikeComponent)
-#     assert result.name == "TestBikeComponent"
-#     # assert result.source == "TestSource"
-#     # assert result.price == 100
-#     # assert result.group == "TestGroup"
+class TestBikeComponentCreate:
+    def test_create_bikecomponent_with_mock_session(self):
+        # Arrange
+        mock_session = Mock()
+        mock_assemblygroupmodule = AssemblyGroupModule(id=1, name="TestAssemblyGroupModule", assemblygroups=[])
+        mock_session.get.return_value = mock_assemblygroupmodule
+
+        input_data = BikeComponentInput(name="TestBikeComponent", source="TestSource", price=100, group="TestGroup")
+        # Act
+        result = create_bikecomponent(id=1, input=input_data, session=mock_session)
+
+        # Assert
+        mock_session.get.assert_called_once_with(AssemblyGroupModule, 1)
+        mock_session.commit.assert_called_once()
+        mock_session.refresh.assert_called_once()
+        assert isinstance(result, BikeComponent)
+        assert result.name == "TestBikeComponent"
+        assert result.source == "TestSource"
+        assert result.price == 100
+        assert result.group == "TestGroup"
